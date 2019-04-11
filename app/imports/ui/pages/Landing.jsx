6b34c16f57b33cc434794f4e8b13976148ec39d9
Ken Tung <kentung@hawaii.edu>
2019-04-10 21:18:18 -1000

    Finished Issue #6
    Landing Page Mockup Done

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,71 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Card, Image, Button, Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        <div className='digits-landing-background'>
+          <Grid container stackable centered columns={3}>
+            <Grid.Column textAlign={'center'}>
+          <Card>
+            <Card.Content>
+              <Card.Header>L&L Hawaiian Barbecue</Card.Header>
+              <Image floated='center' size='medium'
+                     src='https://s3-media2.fl.yelpcdn.com/bphoto/9yZqa-ZIw7nmYKhxdW1_Zw/348s.jpg'/>
+              <Card.Description>
+                Great portions for the price and tastes great! <strong> -Jason Yim</strong>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className='ui buttons'>
+                <Button basic color='green'>
+                  View
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+            </Grid.Column>
+            <Grid.Column textAlign={'center'}>
+          <Card>
+            <Card.Content>
+              <Card.Header>Dunkin Donuts</Card.Header>
+              <Image floated='center' size='medium'
+                     src='https://s3-media1.fl.yelpcdn.com/bphoto/Weh8K8nkw1fCIDicleZltA/348s.jpg'/>
+              <Card.Description>
+                Best place for a quick donut and coffee <strong> -Ken Tung</strong>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className='ui buttons'>
+                <Button basic color='green'>
+                  View
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+            </Grid.Column>
+            <Grid.Column textAlign={'center'}>
+              <Card>
+                <Card.Content>
+                  <Card.Header>Aja Sushi & Bento</Card.Header>
+                  <Image style={{ width: '100%', height: '265px' }} floated='center' size='medium'
+                         src='https://s3-media1.fl.yelpcdn.com/bphoto/QLLIshiOpxYEBrJK5mlMCw/o.jpg'/>
+                  <Card.Description>
+                    Good stuff! <strong> -Philip Johnson</strong>
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui buttons'>
+                    <Button basic color='green'>
+                      View
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
