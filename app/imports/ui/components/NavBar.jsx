13338a9fc7e3a8b07c4a70c0ade9d561f1051e02
cruzjoshua <cruzj4@hawaii.edu>
2019-04-10 23:55:17 -1000

    Finished issue-11.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -16,8 +16,8 @@ class NavBar extends React.Component {
           <Header inverted as='h1'>Ono Manoa Grindz</Header>
         </Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Vendors</Menu.Item>]
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Vendors</Menu.Item>,
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/DaSpot" key='add'>Da Spot</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -8,6 +8,7 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
 import ListVendors from '../pages/ListVendors';
+import DaSpot from '../pages/DaSpot';
 import ListStuffAdmin from '../pages/ListStuffAdmin';
 import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
@@ -28,6 +29,7 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/list" component={ListVendors}/>
+              <ProtectedRoute path="/DaSpot" component={DaSpot}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
