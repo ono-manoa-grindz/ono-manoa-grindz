572e61abe6a37063f0a0002a43d19ea45d4fd9da
Ken Tung <kentung@hawaii.edu>
2019-04-28 23:13:08 -1000

    Update accounts.js

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -4,10 +4,10 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(email, name, password, role) {
+function createUser(email, password, role) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
-    username: name,
+    username: email,
     email: email,
     password: password,
   });
@@ -20,7 +20,7 @@ function createUser(email, name, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ name, email, password, role }) => createUser(name, email, password, role));
+    Meteor.settings.defaultAccounts.map(({ email, password, role }) => createUser(email, password, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
