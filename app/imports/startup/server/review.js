8dffe17df9c429a29312153cb8ecc4c09ee3d869
Jason Yim <jyim3@hawaii.edu>
2019-05-01 20:34:11 -1000

    added default reviews to collection

--- app/imports/startup/server/review.js
+++ app/imports/startup/server/review.js
@@ -3,7 +3,7 @@ import { Reviews } from '../../api/review/review.js';
 
 /** Initialize the database with a default data document. */
 function addData(data) {
-  console.log(`  Adding review for ${data.vendorName} by (${data.owner})`);
+  console.log(`  Adding review for ${data.vendorName} by ${data.owner}`);
   Reviews.insert(data);
 }
 
