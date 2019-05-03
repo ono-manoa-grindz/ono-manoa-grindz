ba32e4fd929f031c65f3898bcca43b4865ae9a38
cruzjoshua <cruzj4@hawaii.edu>
2019-05-02 14:47:38 -1000

    Finished issue-33
    Fixed the footer.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -4,23 +4,31 @@ import { Grid, Icon } from 'semantic-ui-react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
+    const fix = {
+      display: 'block',
+      padding: '10px',
+      height: '20px',
+      width: '100%',
+      background: 'gray',
+    };
     const divStyle = { color: 'white' };
-    const background = { background: 'gray' };
+    const background = { background: 'gray', width: '100%' };
     return (
         <footer>
-          <div style={divStyle} className="footer">
-            <div style={background}>
-              <Grid container stackable centered columns={1}>
-                <div className='footer-text'>
-              <Icon name='copyright outline'/>
-              No Copyright since 2019<br/>
-              Ono Manoa Grindz Inc. All rights not reserved.<br/>
-              University of Hawaii at Manoa<br/>
-              Honolulu, HI 96822
-                </div>
-              </Grid>
+          <div style={fix}/>
+            <div style={divStyle} className="footer">
+              <div style={background}>
+                <Grid container stackable centered columns={1}>
+                  <div className='footer-text'>
+                    <Icon name='copyright outline'/>
+                    No Copyright since 2019<br/>
+                    Ono Manoa Grindz Inc. All rights not reserved.<br/>
+                    University of Hawaii at Manoa<br/>
+                    Honolulu, HI 96822
+                  </div>
+                </Grid>
+              </div>
             </div>
-          </div>
         </footer>
     );
   }
